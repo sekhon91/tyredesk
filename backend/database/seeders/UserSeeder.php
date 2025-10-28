2455323dc4b61b4d6b46505357cf4b27ee7e3ec2
harpreetGr <harpreet@ginger-root.co.uk>
2025-10-28 14:56:13 +0000

    see users of all roles

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -18,11 +18,67 @@ class UserSeeder extends Seeder
       'name' => 'Create Inc',
     ]);
 
-    $user = User::factory()->superAdmin()->create([
+    /** Create Super Admin */
+    $superAdmin = User::factory()->create([
+      'name' => 'Harpreet Sekhon',
+      'email' => 'harpree@createinc.co.uk',
       'company_id' => $company->id,
     ]);
 
     /** Assign Role */
-    $user->assignRole(RolesEnum::SUPERADMIN->value);
+    $superAdmin->assignRole(RolesEnum::SUPERADMIN->value);
+
+    /** 
+     * Create Company Admin 
+     * */
+    $serviceAdmin = User::factory()->create([
+      'name' => 'Company Admin',
+      'email' => 'ca@createinc.co.uk',
+      'company_id' => $company->id,
+    ]);
+
+    /** Assign Role */
+    $serviceAdmin->assignRole(RolesEnum::SERVICEMANAGER->value);
+
+    /** 
+     * Create Service Admin 
+     * */
+    $serviceAdmin = User::factory()->create([
+      'name' => 'Service Manager',
+      'email' => 'sm@createinc.co.uk',
+      'company_id' => $company->id,
+    ]);
+
+    /** Assign Role */
+    $serviceAdmin->assignRole(RolesEnum::SERVICEMANAGER->value);
+
+    /** 
+     * Create Technician users 
+     * */
+    $technician1 = User::factory()->create([
+      'name' => 'Technician One',
+      'email' => 'tech1@createinc.co.uk',
+      'company_id' => $company->id,
+    ]);
+
+    $technician1->assignRole(RolesEnum::TECHNICIAN->value);
+
+    $technician2 = User::factory()->create([
+      'name' => 'Technician Two',
+      'email' => 'tech2@createinc.co.uk',
+      'company_id' => $company->id,
+    ]);
+
+    $technician2->assignRole(RolesEnum::TECHNICIAN->value);
+
+    /** Create Billing Admin */
+    $billingAdmin = User::factory()->create([
+      'name' => 'Billing Admin',
+      'email' => 'ba@createinc.co.uk',
+      'company_id' => $company->id,
+    ]);
+
+    /** Assign Role */
+    $billingAdmin->assignRole(RolesEnum::BILLINGADMIN->value);
   }
 }
